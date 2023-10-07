d565e3836f66a6fd7bb1c49a288c0eb2cda4f2f4
ivesfg1 <wigzfurtado@gmail.com>
2023-10-07 19:27:08 -0300

    corrigindo pra  serviços ficar publico

--- apps/services/api/viewsets.py
+++ apps/services/api/viewsets.py
@@ -3,7 +3,14 @@ from rest_framework import viewsets
 from .serializers import ServiceSerializer
 from ..models import Service
 
+from rest_framework.permissions import AllowAny
+
 
 class ServiceViewset(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [AllowAny()]
+        return super().get_permissions()
