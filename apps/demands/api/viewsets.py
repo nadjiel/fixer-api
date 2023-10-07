c772dadeec9af10635aed8c2d90756964d4ca1e8
ivesfg1 <wigzfurtado@gmail.com>
2023-10-07 13:02:54 -0300

    mudanças na viewset de demanda

--- apps/demands/api/viewsets.py
+++ apps/demands/api/viewsets.py
@@ -3,7 +3,22 @@ from rest_framework import viewsets
 from .serializers import DemandSerializer
 from ..models import Demand
 
+from rest_framework.permissions import AllowAny
 
+
+# TODO: colocar action para apenas administradores poderem performar patch (e o patch ser apenas no status e no motivo)
 class DemandViewset(viewsets.ModelViewSet):
     queryset = Demand.objects.all()
     serializer_class = DemandSerializer
+
+    http_method_names = [
+        "get",
+        "post",
+        "patch",
+    ]
+
+    def get_permissions(self):
+        super().http_method_names
+        if self.action in ["create"]:
+            return [AllowAny()]
+        return super().get_permissions()
