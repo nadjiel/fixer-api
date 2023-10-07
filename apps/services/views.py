37c37e707081b180ab8e50b659d8a1fe30df414e
ivesfg1 <wigzfurtado@gmail.com>
2023-10-07 13:22:28 -0300

    enviando criacao do model Service

--- apps/services/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-
-class Service(View):
-    def get(self, request, *args, **kwargs):
-        # return render(request, "template_name", context)
-        pass
