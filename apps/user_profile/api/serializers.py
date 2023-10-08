83549aec7f6bf99ae4dd37b5479d4c0cd8fadc4c
ivesfg1 <wigzfurtado@gmail.com>
2023-10-08 08:09:13 -0300

    jic

--- apps/user_profile/api/serializers.py
+++ apps/user_profile/api/serializers.py
@@ -8,8 +8,8 @@ from ..models import UserProfile
 
 
 class UserSerializer(serializers.ModelSerializer):
-    phone = serializers.CharField(write_only=True, required=True)
-    picture = serializers.ImageField(write_only=True, required=True)
+    phone = serializers.CharField(write_only=True, required=False)
+    picture = serializers.ImageField(write_only=True, required=False)
     email = serializers.EmailField(write_only=True, required=True)
 
     class Meta:
